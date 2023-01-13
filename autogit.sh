043f3d121ff96aa0901ee178cd358531c1cc31c2
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-01-13 18:04:25 -0300

    problem: buttoms size

--- autogit.sh
+++ autogit.sh
@@ -1,7 +1,4 @@
-git init
 git add -A
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/CarlosViniMSouza/boraCodar-desafio01.git
-git push -u origin main
+git commit -m "problem: buttoms size"
+git push
 git status
